c479e89a9f936d1df598dc7df3dd09ead74743c9
Tim Gymnich <TG908@users.noreply.github.com>
2019-08-18 00:15:07 +0200

    perl env subst

--- keycloak/setup-realm.sh
+++ keycloak/setup-realm.sh
@@ -1,1 +1,1 @@
-perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g' < /tmp/realm-template.json > /tmp/realm-export.json
+perl -pe 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' < /tmp/realm-template.json > /tmp/realm-export.json
